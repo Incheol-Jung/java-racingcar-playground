8b4c944225677ae9ae0c9c0cb5c5d2d5ec8973d5
Incheol Jung <incheol.jung@navercorp.com>
2021-07-19 04:16:34 +0900

    add 자동차의_이름은_5자를_초과할_수_없다

--- src/main/java/CarRacingGame.java
+++ src/main/java/CarRacingGame.java
@@ -1,12 +1,21 @@
-import java.util.Collection;
+import java.util.Arrays;
 
 public class CarRacingGame {
-	private String[] cars; 
+	private String[] cars;
+
 	public void setCars(String carNames) {
-		 cars = carNames.split(",");
+		cars = carNames.split(",");
+		validateCarNames();
 	}
 
 	public String[] getCars() {
-		return cars;		
+		return cars;
+	}
+
+	public void validateCarNames() {
+		boolean isExistInvalidCar = Arrays.stream(cars).anyMatch(car -> car.length() > 5);
+		if(isExistInvalidCar){
+			throw new RuntimeException("5글자 이상으로 설정된 자동차 이름이 있습니다.");
+		}
 	}
 }
--- src/test/java/CarRacingGameTest.java
+++ src/test/java/CarRacingGameTest.java
@@ -7,11 +7,21 @@ public class CarRacingGameTest {
 	void 경주할_자동차의_이름을_선정한다() {
 		// given
 		CarRacingGame carRacingGame = new CarRacingGame();
-		
+
 		// when
 		carRacingGame.setCars("abc,qwe,dsa,qwe,cxz");
-		
+
 		// then
 		assertThat(carRacingGame.getCars().length > 0).isTrue();
+	}
+
+	@Test
+	void 자동차의_이름은_5자를_초과할_수_없다() {
+		// given
+		CarRacingGame carRacingGame = new CarRacingGame();
+
+		// when
+		assertThatThrownBy(() -> carRacingGame.setCars("abc,qwedsadsa,dsa,qwe,cxz")).isInstanceOf(
+			RuntimeException.class);
 	}
 }
